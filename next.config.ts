7bf063f4a318f5dde02a0c09b27f5250ac7c6fd8
Razvalnuy <megamg001@gmail.com>
2025-12-05 01:55:04 +0300

    fix: turbopack scope hoisting для Sanity на Vercel

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
-	/* config options here */
 	// конфиг для изображений чтобы не ругались
 	images: {
 		remotePatterns: [
@@ -10,6 +9,9 @@ const nextConfig: NextConfig = {
 				hostname: "cdn.sanity.io",
 			},
 		],
+	},
+	experimental: {
+		turbopackScopeHoisting: false,
 	},
 }
 
